a2b5f0f59ceba49cac052cff789f58f6dd5f8567
Vitaly Kopytich <kopytich.vitaly@gmail.com>
2017-03-05 15:33:29 +0300

    The second task

--- dp/lab2/lab2_2.php
+++ dp/lab2/lab2_2.php
@@ -1,2 +1,21 @@
 <?php
 
+function find_number($sum, $size) {
+	$res = 0;
+
+	if($sum < 0) return 0;
+
+	if($size == 1) {
+		if($sum > 9) return 0;
+		else return 1;
+	} else {
+		for($i=0; $i < 10; $i++)
+			$res += find_number($sum-$i, $size-1);
+	}
+	return $res;
+}
+
+$size = 7;
+$sum = 13;
+
+echo find_number($sum, $size);
